028616470211422c4a1dc072fef81e921dadd06b
mmschmitz <mmschmitz@alumni.emory.edu>
2022-03-13 22:59:50 -0400

    Adding initial take at python code, but I haven't done anything with this

---
+++ python_code.py
@@ -0,0 +1,12 @@
+#https://pyimagesearch.com/2021/05/12/opencv-edge-detection-cv2-canny/
+#https://pyimagesearch.com/2018/09/19/pip-install-opencv/
+
+#import new packages
+import sys
+import os
+import cv2 #opencv
+
+os.chdir("C:\Users\mmsch\OneDrive\Desktop\pet_project\cute_photos")
+photo_directory = os.getcwd()
+#print(photo_directory)
+
