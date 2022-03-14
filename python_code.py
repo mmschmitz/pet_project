b9f92db82718ee7d796d014e74d72980c5d18c70
mmschmitz <mmschmitz@alumni.emory.edu>
2022-03-13 23:11:34 -0400

    Added some additional modules and put in an initial image

--- python_code.py
+++ python_code.py
@@ -4,9 +4,15 @@
 #import new packages
 import sys
 import os
-import cv2 #opencv
+#import imageio as iio
+#import opencv-contrib-python as cv2 #opencv
+import cv2
+import argparse
 
 os.chdir("C:\Users\mmsch\OneDrive\Desktop\pet_project\cute_photos")
 photo_directory = os.getcwd()
 #print(photo_directory)
 
+#initial test with a single image (will make a pipeline for the photo directory later)
+img_test = cv2.imread("20200516_184555.jpg")
+cv2.imshow("image_test1",img_test)
